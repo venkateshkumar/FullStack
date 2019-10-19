bda1b51947a20755940584f80398bbe07291bf37
svenkateshkumar <venkateshkumarsr@gmail.com>
2019-10-20 00:05:13 +0400

    Base Project

---
+++ FullStackData/src/main/java/com/fullstackdata/FullStackData/FullStackDataApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackdata.FullStackData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDataApplication.class, args);
+	}
+
+}
---
+++ FullStackData/src/test/java/com/fullstackdata/FullStackData/FullStackDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackdata.FullStackData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
